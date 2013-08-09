ed238288ffa5e1a5198600b976d4b3238a8233ef
Brad Garland <brad@bradgarland.com>
2013-08-10 09:10:04 +1000

    load sample files; merge multiple language files

--- guess_lang.rb
+++ guess_lang.rb
@@ -3,4 +3,29 @@ class GuessLang
   def self.extract_vocab(text)
     text.downcase.gsub(/[.,;:]/,'').split(' ').uniq
   end
+
+  def self.load_samples(filenames)
+    samples = Hash.new
+    filenames.each do |filename|
+      samples[filename] = self.read_file(filename)
+    end
+    samples
+  end
+
+  def self.read_file(filename)
+    File.open(filename).read
+  end
+
+  def self.merge_vocabs(vocabs)
+    merged_vocabs = Hash.new
+    vocabs.each do |language_key, vocab|
+      language = language_key.downcase.split('.')[0] # language is stuff before "."
+      if merged_vocabs[language]
+        merged_vocabs[language] = merged_vocabs[language] | vocab
+      else
+        merged_vocabs[language] = vocab
+      end
+    end
+    merged_vocabs
+  end
 end
--- spec/guess_lang_spec.rb
+++ spec/guess_lang_spec.rb
@@ -3,13 +3,42 @@ require File.join(__dir__, '..', 'guess_lang')
 describe GuessLang, 'extract_vocab' do
   context 'when given sample text' do
     it 'should return the unique words in the text' do
-      GuessLang.extract_vocab('words words that are not unique unique').should == %w/words that are not unique/
+      expect(GuessLang.extract_vocab('words words that are not unique unique')).to eq(%w/words that are not unique/)
     end
     it 'should ignore punctuation' do
-      GuessLang.extract_vocab('words, words: and more words; words.').should == %w/words and more/
+      expect(GuessLang.extract_vocab('words, words: and more words; words.')).to eq(%w/words and more/)
     end
     it 'should ignore case' do
-      GuessLang.extract_vocab('words Words WORDS').should == %w/words/
+      expect(GuessLang.extract_vocab('words Words WORDS')).to eq(%w/words/)
+    end
+  end
+end
+
+describe GuessLang, 'load_samples' do
+  context 'when there is one language file' do
+    before do
+      @contents = 'sample contents'
+      @files = ['english.1']
+      GuessLang.stub(:read_file).and_return(@contents)
+    end
+    after do
+      GuessLang.load_samples(@files)
+    end
+    it 'should return the words in the file indexed by language file name' do
+      expect(GuessLang.load_samples(@files)).to eq({'english.1' => @contents})
+    end
+  end
+end
+
+describe GuessLang, 'merge_vocabs' do
+  context 'when there are multiple files for the same language' do
+    before do
+      @vocabs = {'lang.1' => %w/a b c    /,
+                 'lang.2' => %w/    c d e/
+                }
+    end
+    it 'should combine them into a single vocab' do
+      expect(GuessLang.merge_vocabs(@vocabs)).to eq({'lang' => %w/a b c d e/})
     end
   end
 end
